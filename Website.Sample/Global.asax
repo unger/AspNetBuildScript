735001b0dffbe88b433446f62c42ab4ffbd23545
Magnus Unger <magnus_unger@hotmail.com>
2015-05-21 02:52:04 +0200

    Added sample project and build script

---
+++ Website.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Website.Sample.MvcApplication" Language="C#" %>
---
+++ build.cmd
@@ -0,0 +1,49 @@
+@echo Off
+
+set solutionName=
+for %%i in (*.sln) do set solutionName=%%i
+
+if "%solutionName%" == "" (
+	Echo No Solution file found in folder
+	Exit /b
+)
+
+set config=%1
+if "%config%" == "" (
+	set config=Release
+)
+
+for /f "tokens=1-3 delims=-" %%a in ("%DATE%") do (set mydate=%%a%%b%%c)
+for /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (set mytime=0%%a%%b)
+SET mytime=%mytime:~-4%
+
+set buildTempDir=Build\Temp
+set buildOutDir=Build\%config%
+set buildFullOutDir=%buildOutDir%\Full\%mydate%_%mytime%
+set buildDiffOutDir=%buildOutDir%\Differential\%mydate%_%mytime%
+
+
+nuget restore
+msbuild %solutionName% /t:Rebuild /p:Configuration=%config%;OutDir="%cd%\%buildTempDir%";UseWPP_CopyWebApplication=True;PipelineDependsOnBuild=False
+
+set webProjectOutputFolder=
+for /d %%i in (%buildTempDir%\_PublishedWebsites\*) do set webProjectOutputFolder=%%i
+
+if not "%webProjectOutputFolder%" == "" (
+
+	set buildLastFullDir=
+	for /d %%i in (%buildOutDir%\Full\*) do set buildLastFullDir=%%i
+
+	if not "%buildLastFullDir%" == "" (
+		robocopy %buildLastFullDir%\Site %buildDiffOutDir%\Temp /MIR /A-:A /R:5 /W:1 /FFT
+		robocopy %webProjectOutputFolder% %buildDiffOutDir%\Temp /S /XO /A+:A /R:5 /W:1 /FFT
+		robocopy %buildDiffOutDir%\Temp %buildDiffOutDir%\Site /S /A /R:5 /W:1 /FFT
+		rmdir %buildDiffOutDir%\Temp /s /q
+	)
+
+	robocopy %webProjectOutputFolder% %buildFullOutDir%\Site /MIR /R:5 /W:1 /FFT
+
+	REM Zip output folder
+	powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('%buildFullOutDir%\Site', '%buildFullOutDir%_full.zip'); }"
+)
+
